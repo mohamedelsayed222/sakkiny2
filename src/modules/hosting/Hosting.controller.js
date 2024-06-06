c0fba555444deae6fc241f72514698b3117474d7
Mohamed Elsayed <ms6701937@gmail.com>
2024-06-06 05:28:13 +0300

    test51

--- src/modules/hosting/Hosting.controller.js
+++ src/modules/hosting/Hosting.controller.js
@@ -26,14 +26,14 @@ const {
 if(!user.isVerified){
     return next (new Error("Please verify your identity",{cause:200}))
 }
-// if (!req.files?.length||req.files.length<5){
-//     return next (new Error("Please upload  at least 5 pictures of your property",{cause:200}))
-// }
-// if(!user.customId){
-//   const customId = nanoid()
-//   user.customId=customId
-//   await user.save()
-// }
+if (!req.files?.length||req.files.length<5){
+    return next (new Error("Please upload  at least 5 pictures of your property",{cause:200}))
+}
+if(!user.customId){
+  const customId = nanoid()
+  user.customId=customId
+  await user.save()
+}
 const essentials={}
 if(details){
 // console.log(details);
@@ -45,21 +45,21 @@ for(const ele of detailsarr){
 }
 }
 // console.log(essentials);
-// const customId = nanoid()
-// const propertyImages = []
-// const publicIds = []
-// const propertyFolder=`${process.env.PROJECT_FOLDER}/user/${user.customId}/Property/${customId}`
+const customId = nanoid()
+const propertyImages = []
+const publicIds = []
+const propertyFolder=`${process.env.PROJECT_FOLDER}/user/${user.customId}/Property/${customId}`
 
-// for (const file of req.files) {
-//   const { secure_url, public_id } = await cloudinary.uploader.upload(
-//     file.path,
-//     {
-//       folder:propertyFolder 
-//     },
-//   )
-//   propertyImages.push({ secure_url, public_id })
-//   publicIds.push(public_id)
-// }
+for (const file of req.files) {
+  const { secure_url, public_id } = await cloudinary.uploader.upload(
+    file.path,
+    {
+      folder:propertyFolder 
+    },
+  )
+  propertyImages.push({ secure_url, public_id })
+  publicIds.push(public_id)
+}
 const type=req.body.type.toLowerCase();
 const per=req.body.per?.toLowerCase();
 const property=await propertyModel.create(
@@ -77,16 +77,16 @@ const property=await propertyModel.create(
     location,
     essentials,
     addedBy:user._id,
-    // propertyImages,
-    // customId
+    propertyImages,
+    customId
     // SurroundingFacilities,
 })
 if (!property) {
-  // await cloudinary.api.delete_resources(publicIds)
-  // await cloudinary.api.delete_folder(propertyFolder)
+  await cloudinary.api.delete_resources(publicIds)
+  await cloudinary.api.delete_folder(propertyFolder)
   return next(new Error('try again later', { cause: 404 }))
 }
-res.status(200).json({status:true, message: 'Done', property })
+res.status(200).json({status:true, message: 'Done' })
 }
 
 
