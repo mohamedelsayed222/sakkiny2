bb93a13dfe54ee055bb3a8079f16caff36051026
Mohamed <ms6701937@gmail.com>
2024-08-10 20:17:32 +0300

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import  bootstrap  from './src/index.router.js';
 import dotenv from 'dotenv'
 dotenv.config()
 const app=express();
+////
 const port =process.env.Port_Number || 5000;
 bootstrap(app,express)
 app.listen(port,()=>{
